461b79e80470527deb6ab7f877f068cc96032e6a
Matthew Ronchetto <hello@doamatto.xyz>
2022-08-15 13:02:18 -0700

    chore: add rest of codes

--- main.go
+++ main.go
@@ -11,6 +11,12 @@ import (
 
 	"github.com/boombuler/barcode"
 	"github.com/boombuler/barcode/aztec"
+	//"github.com/boombuler/barcode/codabar"
+	"github.com/boombuler/barcode/code39"
+	"github.com/boombuler/barcode/code93"
+	"github.com/boombuler/barcode/code128"
+	"github.com/boombuler/barcode/ean"
+	"github.com/boombuler/barcode/pdf417"
 	"github.com/boombuler/barcode/qr"
 )
 
@@ -49,7 +55,45 @@ func stdout(w http.ResponseWriter, data barcode.Barcode) {
 func main() {
 	http.HandleFunc("/aztec/", func(w http.ResponseWriter, r *http.Request) {
 		data := handleData(r.URL.Path[len("/aztec/"):])
-		code, _ := aztec.Encode([]byte(data), 33, 0)
+		code, _ := aztec.Encode([]byte(data), 33, 0) // Default values but pinned
+		code, _ = barcode.Scale(code, 500, 500)
+		stdout(w, code)
+	})
+	/*http.HandleFunc("/coda/", func(w http.ResponseWriter, r *http.Request) {
+		data := handleData(r.URL.Path[len("/coda/"):])
+		code, _ := codabar.Encode(data)
+		code, _ = barcode.Scale(code, 500, 250)
+		stdout(w, code)
+	})*/
+	http.HandleFunc("/39/", func(w http.ResponseWriter, r *http.Request) {
+		data := handleData(r.URL.Path[len("/39/"):])
+		code, _ := code39.Encode(strings.ToUpper(data), true, true)
+		scaledCode, _ := barcode.Scale(code, 500, 100)
+		stdout(w, scaledCode)
+	})
+	http.HandleFunc("/93/", func(w http.ResponseWriter, r *http.Request) {
+		data := handleData(r.URL.Path[len("/93/"):])
+		code, _ := code93.Encode(strings.ToUpper(data), true, true)
+		code, _ = barcode.Scale(code, 500, 100)
+		stdout(w, code)
+	})
+	http.HandleFunc("/128/", func(w http.ResponseWriter, r *http.Request) {
+		data := handleData(r.URL.Path[len("/128/"):])
+		code, _ := code128.Encode(strings.ToUpper(data))
+		scaledCode, _ := barcode.Scale(code, 500, 100)
+		stdout(w, scaledCode)
+	})
+	http.HandleFunc("/ean/", func(w http.ResponseWriter, r *http.Request) {
+		data := handleData(r.URL.Path[len("/ean/"):])
+		// TODO: convert data to int; if fail, report that data is no good
+		// TODO: EAN can only do numbers, 16 of them iirc
+		code, _ := ean.Encode(data)
+		scaledCode, _ := barcode.Scale(code, 500, 250)
+		stdout(w, scaledCode)
+	})
+	http.HandleFunc("/417/", func(w http.ResponseWriter, r *http.Request) {
+		data := handleData(r.URL.Path[len("/417/"):])
+		code, _ := pdf417.Encode(strings.ToUpper(data), 4)
 		code, _ = barcode.Scale(code, 500, 500)
 		stdout(w, code)
 	})
